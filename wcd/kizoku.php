d29f8a873bc3d08884ad80d88b3ec44276b08540
Kotaro <gemini.522.kotaro@gmail.com>
2015-11-30 09:40:55 +0900

    wcd

---
+++ wcd/kizoku.php
@@ -0,0 +1,41 @@
+<!doctype html>
+<html lang="ja">
+<head>
+<meta charset="UTF-8">
+<title>貴族の食卓</title>
+<!--[if lt IE 9]>
+<script type="text/javascript" src="http://html5shim.googlecode.com/svn/trunk/html5.js"></script>
+<![endif]-->
+<script>
+  (function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
+  (i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
+  m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
+  })(window,document,'script','//www.google-analytics.com/analytics.js','ga');
+
+  ga('create', 'UA-70707698-2', 'auto');
+  ga('send', 'pageview');
+
+</script>
+<link href="kizoku.css" rel="stylesheet" type="text/css">
+</head>
+
+<body>
+
+
+
+
+<article>
+<h1>半透明の美しいパネル</h1>
+<p><?php echo 'hello, world'; ?>
+洋酒といえば、誰でも最初に思い浮かべるのがウイスキー。いわば洋酒のシンボル的な存在なのだが、英語表記が［一般に〔米〕では Whiskey,〔英〕では Whisky.］であることはあまり知られていない。米英両国では、このスペルの差で自国産と輸入品を区別しているという。わが和製ウイスキーの“Whisky”という英国式表示は、手本にしたスコッチのフォルムに倣ったものであり、それ以上の意味はないようだ。カナ表記にしても、ごくまれに〈ウヰスキー〉という書き方を見かけるが、これとて差別化を意図したものではなく単にカナづかいの時代性にすぎない。
+　現在、カナ表記は〈ウイスキー〉に一本化しており、そこに国産・舶来の区別はない。最近は価格面での差もちぢまり、20年前に ￥10,000 だった本場のスコッチが半値近くになるなど、ウイスキーは一層ポピュラーなものになってきた。しかし、消費量についていえば、洋酒のトップは何といってもビールだろう。よっぽどの下戸はともかく、まず誰でもタシナミ程度には口にする。</p>
+</article>
+ 
+<svg version="1.1" xmlns="http://www.w3.org/2000/svg">
+ <filter id="blur">
+   <fegaussianblur stdDeviation="6" />
+   </filter>
+</svg>
+
+</body>
+</html>
---
+++ wcd/tetetest.php
@@ -0,0 +1,11 @@
+<!doctype html>
+<html>
+<head>
+<meta charset="UTF-8">
+<title>無題ドキュメント</title>
+</head>
+
+<body>
+<p>The time is now <?php echo 'Hello, world'; ?>.</p>
+</body>
+</html>
